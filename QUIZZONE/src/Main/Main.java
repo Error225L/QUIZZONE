c689398a36149518a2a36f8ca20611b4bcdbe9aa
Psycho <Psycho@Psycho-PC>
2017-11-02 09:47:48 +0100

    sf

---
+++ QUIZZONE/src/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+
+import Control.ControllerClient;
+import Model.GestioneClient;
+import View.Finestra;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Finestra f=new Finestra();
+		GestioneClient g=new GestioneClient();
+		ControllerClient c=new ControllerClient();
+	}
+
+}
